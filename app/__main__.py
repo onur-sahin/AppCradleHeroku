0f3067767b0b9270bc9cad51f94ceb6636c902d5
onur-sahin <onur2503@protonmail.com>
2022-09-22 13:14:04 +0100

    depo

--- app/__main__.py
+++ app/__main__.py
@@ -1,14 +1,9 @@
 
 
-import wave
 from flask import Flask, request, jsonify
 from app.utils import transform_audio
 from app.cry_detection.createModel      import is_it_crying
 from app.cry_classification.createModel import classification
-import io
-import soundfile
-import numpy as np
-import torch
 
 from typing import BinaryIO
 
@@ -34,57 +29,17 @@ def index():
 def predict():
 
     file:BinaryIO = request.files.get('file')
-    
-    data = request.data
 
-    if (data==None and file==None):
-        return jsonify({"erorr": "NO FILE OR DATA"})
+    if file is None or file.filename == "":
+        return jsonify({"erorr": "NO FILE"})
 
-    elif file != None:
-
-        if file.filename == "":
-            return jsonify({"erorr": "NO FILE"})
-
-        if not allowed_file(file.filename):
-            return jsonify({"error": "FORMAT NOT SUPPORTED"})
-
-        
-        audio_bytes:BinaryIO = file.read()
-
-        sig:tuple[np.ndarray, int] = soundfile.read( io.BytesIO(audio_bytes), dtype='float32' )
-
-        waveform:torch.Tensor = torch.from_numpy(sig[0].transpose())
-        sr:int = sig[1]
-
-        sig = (waveform, sr)
-
-    elif data != None:
-
-        temp = io.BytesIO()
-        
-        wave_write = wave.open(temp, "wb")
-
-        wave_write.setnchannels(1)        # number of channels  - mono channel
-        wave_write.setsampwidth(2)        # sample width in bytes
-        wave_write.setframerate(22050)    # sampling rate in Hz
-
-        wave_write.writeframes(data)
-
-        wave_write.close()
-        temp.seek(0)
-
-        sig =  soundfile.read(temp, dtype="float32")
-
-        waveform:torch.Tensor = torch.from_numpy(sig[0].transpose())
-        sr:int = sig[1]
-
-        sig = (waveform, sr)
-
+    if not allowed_file(file.filename):
+        return jsonify({"error": "FORMAT NOT SUPPORTED"})
 
     try:
-        
+        audio_bytes = file.read()
 
-        spectrom = transform_audio(sig)
+        spectrom = transform_audio(audio_bytes)
 
         outputs_detection = is_it_crying(spectrom)
 
